4be24bffc666e24d90a491f299c8258cbb1bf1d8
Austin Scrivener <ostim12121212@gmail.com>
2024-11-24 21:25:38 +1300

    fixed things

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const swapBTN = document.getElementById("swap-btn")
 const wolfImage = document.getElementById("wolfImage")
 
 let wolfImgers =["forest", "mountain","prairie"]
+let wolfAltText =["wolf in a forest", "wolf on top of a mountain", "wolf in a prairie"]
 let wolfnumber = 0
 
 swapBTN.addEventListener("click", function (){
@@ -9,6 +10,7 @@ swapBTN.addEventListener("click", function (){
     if (wolfnumber == 3){
       wolfnumber =0
     }
-    wolfImage.src = "/img/" + wolfImgers[wolfnumber] + "-wolf.webp"
+    wolfImage.src = "img/" + wolfImgers[wolfnumber] + "-wolf.webp"
+    wolfImage.alt = wolfAltText[wolfnumber]
   }
 )
